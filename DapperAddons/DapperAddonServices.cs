1f95245e947482cf58b2b7e625e59509aed09f60
Ahsan Joiya <iamAhsanJoiya@outlook.com>
2022-06-14 09:50:16 +0500

    Initial Release

---
+++ DapperAddons/DapperAddonServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace DapperAddons;
+public static class DapperAddonServices
+{
+    public static IServiceCollection AddDapperAddon(this IServiceCollection services)
+    {
+        services.TryAddScoped<IDbHelpers, DbHelpers>();
+        return services.AddDapperAddon();
+    }
+}
---
+++ DapperAddons/IDbHelpers.cs
@@ -0,0 +1,16 @@
+﻿
+namespace DapperAddons;
+
+public interface IDbHelpers
+{
+    Task<int> DeleteAsync<InputParemeters>(string sqlQuery, InputParemeters? inputParameters = default, string connectionID = "DefaultConnection");
+    Task<int> DeleteByStoreProcedureAsync<InputParemeters>(string storeProcedure, InputParemeters? inputParameters = default, string connectionID = "DefaultConnection");
+    Task<List<ReturnType>> GetAllAsync<ReturnType, InputParemeters>(string sqlQuery, InputParemeters? inputParameters = default, string connectionID = "DefaultConnection");
+    Task<List<ReturnType>> GetAllByStoreProcedureAsync<ReturnType, InputParemeters>(string storeProcedure, InputParemeters? inputParameters = default, string connectionID = "DefaultConnection");
+    Task<ReturnType> GetOneAsync<ReturnType, InputParemeters>(string sqlQuery, InputParemeters? inputParameters = default, string connectionID = "DefaultConnection");
+    Task<ReturnType> GetOneByStoreProcedureAsync<ReturnType, InputParemeters>(string storeProcedure, InputParemeters? inputParameters = default, string connectionID = "DefaultConnection");
+    Task<int> InsertOneAsync<InputParemeters>(string sqlQuery, InputParemeters? inputParameters = default, string connectionID = "DefaultConnection");
+    Task<int> InsertOneByStoreProcedureAsync<InputParemeters>(string storeProcedure, InputParemeters? inputParameters = default, string connectionID = "DefaultConnection");
+    Task<int> UpdateOneAsync<InputParemeters>(string sqlQuery, InputParemeters? inputParameters = default, string connectionID = "DefaultConnection");
+    Task<int> UpdateOneByStoreProcedureAsync<InputParemeters>(string storeProcedure, InputParemeters? inputParameters = default, string connectionID = "DefaultConnection");
+}
